1a872ee1848d1feab1b54dbba0a0b0a46c9d8343
Rudra Patel <83312632+Alphaknite@users.noreply.github.com>
2024-03-16 12:52:41 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button"
 import "./index.css"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl text-center">Hello, World!</h1>
+      <Button>Click me</Button>
+
     </>
   )
 }
