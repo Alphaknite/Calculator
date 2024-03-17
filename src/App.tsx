3c1f6df7cbececc55d9389626fa4bba7e7162c8a
Rudra Patel <83312632+Alphaknite@users.noreply.github.com>
2024-03-16 22:17:17 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Button } from "@/components/ui/button"
 import "./index.css"
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 function App() {
 
   return (
     <>
-      <Button>Click me</Button>
+    <div className="flex w-screen h-screen justify-center items-center "> 
+        <Card className="w-[350px] flex flex-col">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>Deploy your new project in one-click.</CardDescription>
+          </CardHeader>
+          <div className="flex justify-center">
+            <Button className="flex justify-center w-2/3 ">Calculate</Button>
+          </div>
+        </Card>
+      </div>
+
 
     </>
   )
